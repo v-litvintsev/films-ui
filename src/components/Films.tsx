e801b397b5f024758aac4f3a3677e9824015cad5
Vladimir Litvincev <vl.litvintsev@yandex.ru>
2022-06-06 18:34:50 +0700

    fixes

--- src/components/Films.tsx
+++ src/components/Films.tsx
@@ -10,7 +10,7 @@ const Films: FC<IProps> = ({ films }) => {
     <>
       <ul className="list-none space-y-4 text-4xl font-bold mb-3">
         {films &&
-          films.data?.map((film) => {
+          films.data.map((film) => {
             return (
               <li key={film.id}>
                 <Link href={`/film/${film.attributes.slug}`}>
